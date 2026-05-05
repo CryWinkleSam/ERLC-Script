69e253135baa9c38f8b792fe8269c76ec3e5fc76
CryWinkleSam <uncync82@gmail.com>
2026-05-04 22:55:27 -0700

    Create emergency response liberty county.lua

---
+++ emergency response liberty county.lua
@@ -0,0 +1,154 @@
+-- ERX ER:LC Xeno & Solara wont work, USE Madium - discord.gg/GujAr5Dcfe
+
+-- ⚠️⚠️ NEVER Use main accounts people WILL MOST LIKELY report you as your using a ragehack so you will probably be manually banned by PRC Staff! ⚠️⚠️
+-- Altgen(s): Resort (discord.gg/yaBTT4Wcv) | Patware (discord.gg/fAknSwn6hr)
+
+-- Key: https://ads.luarmor.net/get_key?for=ERXKey__ERLC-lWEEVBONdthV
+-- Support & Discord: https://discord.gg/rKK6AVfZmQ [.gg/erlchacks]
+
+-- Tutorial: https://www.youtube.com/watch?v=dcf7SIMvjeY
+
+script_key="YOUR_KEY_IN_QUOTES";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e72dda22a300c4de5ded1a43123b0e20.lua"))()
+
+--[[
+
+Features and all supported executors below!
+
+[Features Probably not up to date check discord! https://discord.gg/87B2chEJ6U & discord.gg/erlchacks]
+
+👑 = Premium
+✅ = Freemium
+
+Buy Premium ERX in the discord! 
+
+# ERX [ERLC] Features
+
+[Main] Tab
+[+] Free Instant Refuel ✅
+[+] Infinite Stamina ✅
+[+] No Fall Damage ✅
+[+] Instant Respawn ✅
+[+] Anti Arrest [respawn on cuff]✅
+[+] Arrest Aura 👑
+[+] Auto Arrest All 👑
+[+] Auto Arrest All ServerHop 👑
+[+] Respawn ✅
+
+[LocalPlayer] Tab
+[+] Infinite Jump ✅
+[+] Walk Speed ✅
+[+] Noclip ✅
+[+] Anti Void ✅
+[+] Fly ✅
+[+] Money Aura ✅
+[+] Emote Player ✅
+[+] Get Basketball ✅
+
+[Players] Tab
+[+] Admin CMD Notifier ✅ [NEEDS Chat]
+[+] Chat Spy ✅ [NEEDS Chat]
+[+] On View Actions ✅ [NEEDS Chat]
+[Kill, Respawn, SelfFling on view]
+[+] Custom ERX Chat ✅
+
+[Trolling] Tab
+[+] Make Closest Vehicle Dirt 👑
+[+] Pop All Tires 👑
+[+] Walk Fling ✅
+[+] Car Fling ✅
+[+] Fake Mod Calls 👑
+[+] Anti Cuff 👑 [You legit just cannot be cuffed]
+[+] Punch Aura ✅
+[+] Invisibility ✅
+[+] Fling All 👑
+[+] Shock Earrape 👑
+[+] Detain Crasher 👑
+
+[Visuals] Tab
+[+] Instant Interact 👑
+[+] Freecam ✅
+[+] No Fog ✅
+[+] Fake PRC HeadTag ✅
+[+] Change Game Weather ✅
+[+] Change Game Time ✅
+[+] Fake Chat [Client]✅
+[+] ESP ✅
+
+[Vehicle Mods] Tab
+[+] Infinite Fuel ✅
+[+] Earrape 👑
+[+] Horn Boost ✅
+[+] Speed Multiplier ✅
+[+] Instant Brake ✅
+[+] Extend Pit Hitbox 👑
+[+] Vehicle Noclip ✅
+[+] Anti Traffic Lights✅
+[+] Anti Speed Traps ✅
+[+] Anti Stop Sticks ✅
+[+] No Collision ✅
+[+] Destroy All Poles ✅
+[+] Open ModShop ✅
+[+] Unstuck Car ✅
+
+[Gun Mods] Tab
+[+] No Projectile Fails 👑
+[+] FireRate Modifier ✅
+[+] Instant Reload ✅
+[+] Auto Refill ✅
+
+[Aimbot] Tab
+[+] Silent Aim ✅
+[+] No Recoil ✅
+[+] Wallbang ✅
+
+[Robberies] Tab
+[+] Anti Robbery Fails ✅
+[+] Auto Crowbar ✅
+[+] AutoFish Farm 👑
+[+] AutoRob 👑
+
+[Automation] Tab
+[+] Buy Gun & Gear ✅
+[+] DOT XP Farm ✅
+[+] FD XP Farm ✅
+
+[Teleports] Tab
+[+] Teleport to a set Location ✅
+[+] Teleport to a Bounty Vehicle ✅
+[+] Map Waypoint Teleport ✅
+
+[Settings] Tab
+[+] Toggle Visibility ✅
+[+] StreamingEnabled Bypass ✅
+[+] Autorob Webhook Info 👑
+
+[Chat Commands]
+[+] :load command 👑
+[+] :tocar command 👑
+[+] :to command 👑
+
+[Official Custom Features] needs premium & you need to buy these all seperatly in the dc .gg/erlchacks
+[+] Delay Private Server Bans/Kicks 
+[+] Car Stealer
+[+] Server Crasher
+[+] FE Force Chat
+
+Supported Executors
+
+PC:
+- Synapse Z (discord.gg/synz)
+- Potassium (https://potassium.pro | discord.gg/potassium)
+- Madium (discord.gg/olemad)
+- Ronix (discord.gg/ronixstudios)
+
+Mobile:
+- Delta (discord.gg/deltax)
+
+Mac:
+- OpiumWare (discord.gg/hKrrz4bW)
+- Macsploit (unknown)
+
+.gg/erlchacks
+
+]] 
